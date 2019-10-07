6f4e1ff05cd2a961c319b4dd326741699bc0d96d
Romeo David <zapperhatz26@gmail.com>
2019-10-07 21:46:54 +0800

    INITIAL COMMIT

---
+++ application/logs/log-2019-10-07.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-07 03:10:25 --> 404 Page Not Found: Search/adminsearch
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-07 03:10:25 --> 404 Page Not Found: Search/adminsearch
+ERROR - 2019-10-07 03:10:26 --> 404 Page Not Found: Search/adminsearch
+ERROR - 2019-10-07 14:57:53 --> 404 Page Not Found: Public/js
+ERROR - 2019-10-07 14:57:53 --> 404 Page Not Found: Public/js
+ERROR - 2019-10-07 14:59:18 --> 404 Page Not Found: Public/js
+ERROR - 2019-10-07 15:45:51 --> 404 Page Not Found: Public/js
